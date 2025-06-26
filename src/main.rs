205421c44d319c094d1f16b6a888b5637c03b99f
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-06-26 18:48:01 +0200

    fix transparency

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,17 @@ use crossterm::{
 };
 use image::{GenericImageView, guess_format};
 
+fn rgba_to_color(rgba: [u8; 4]) -> crossterm::style::Color {
+    match rgba[3] {
+        0 => crossterm::style::Color::Reset,
+        _ => crossterm::style::Color::Rgb {
+            r: rgba[0],
+            g: rgba[1],
+            b: rgba[2],
+        },
+    }
+}
+
 fn main() {
     let mut args = args();
     let Some(image_path) = args.nth(1) else {
@@ -31,19 +42,26 @@ fn main() {
     let mut stdout = stdout();
     for y in (0..image.height()).step_by(2) {
         for x in 0..image.width() {
-            let top_pixel = buf[y as usize][x as usize].unwrap().0;
-            let top_color = crossterm::style::Color::Rgb {
-                r: top_pixel[0],
-                g: top_pixel[1],
-                b: top_pixel[2],
-            };
+            let top_rgba = buf[y as usize][x as usize].unwrap().0;
+            let top_color = rgba_to_color(top_rgba);
 
-            let bottom_pixel = buf[y as usize + 1][x as usize].unwrap().0;
-            let bottom_color = crossterm::style::Color::Rgb {
-                r: bottom_pixel[0],
-                g: bottom_pixel[1],
-                b: bottom_pixel[2],
-            };
+            let bottom_rgba = buf[y as usize + 1][x as usize].unwrap().0;
+            let bottom_color = rgba_to_color(bottom_rgba);
+
+            if top_rgba[3] == 0 {
+                if bottom_rgba[3] == 0 {
+                    // if both pixels are empty
+                    queue!(stdout, ResetColor).unwrap();
+                    print!(" ");
+                    continue;
+                }
+                // if only top pixel is empty
+                queue!(stdout, SetForegroundColor(bottom_color)).unwrap();
+                print!("▄");
+                continue;
+            }
+
+            // if neither pixel is empty
             queue!(stdout, SetBackgroundColor(bottom_color)).unwrap();
             queue!(stdout, SetForegroundColor(top_color)).unwrap();
             print!("▀");
