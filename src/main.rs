52176244b776bdaea06356fbc6ce15c7f409e2cf
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-06-26 19:00:17 +0200

    better error messages and comments

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,27 @@ use image::guess_format;
 fn main() {
     let mut args = args();
     let Some(image_path) = args.nth(1) else {
-        println!("specify image path");
+        println!("usage: hamis <image path>");
         return;
     };
-    let data = std::fs::read(image_path).unwrap();
-    let format = guess_format(&data).unwrap();
 
+    // read file data to buffer
+    let data = match std::fs::read(image_path) {
+        Ok(data) => data,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+    // guess image format of file specified
+    let Ok(format) = guess_format(&data) else {
+        println!("couldn't infer format of image");
+        return;
+    };
+
+    // image::load needs BufRead + Seek, so i wrap file buffer in a cursor
     let cursor = Cursor::new(data);
+
     let image = image::load(cursor, format).unwrap();
 
     draw_image(&image);
